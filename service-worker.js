7bd71ee5e9903d612a66871e85efa3df36fef01e
eliasgoette <elias63815@gmail.com>
2021-05-15 11:40:51 +0200

    PWA now ready to deploy

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const cacheName = "PWACache";
+const staticAssets = [
+    './',
+    './index.html',
+    './service-worker.js',
+    './manifest.webmanifest'
+];
+
+self.addEventListener('install', async e => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssets);
+    return self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    const req = e.request;
+    const url = new URL(req.url);
+
+    if(url.origin === location.origin){
+        e.respondWith(cacheFirst(req));
+    } else{
+        e.respondWith(networkAndCache(req));
+    }
+});
+
+async function cacheFirst(req){
+    const cache = await caches.open(cacheName);
+    const cached = await cache.match(req);
+    return cached || fetch(req);
+}
+
+async function networkAndCache(req){
+    await caches.open(cacheName);
+    try {
+        const fresh = await fetch(req);
+        await cache.put(req, fresh.clone());
+        return fresh;
+    } catch (e) {
+        const cached = await cache.match(req);
+        return cached;
+    }
+}
